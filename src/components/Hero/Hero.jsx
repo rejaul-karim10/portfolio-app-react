b2b3153bf9930ac1d9164228a7aca065f59c8aa8
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-01-27 22:48:00 +0800

    adding hero banner

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Hero = () => {
   return (
-    <div className='bg-[#0a192f]'>
-
+    <div className="bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto p-8 flex flex-col justify-center h-screen">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-white">Rejaul Karim Rabbi</h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I am a full stack developer</h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a full stack developer specializing in building (and occasionally
+          designing) exeptionally digital experience. i am focused on building
+          responsive full stack application
+        </p>
+        <div>
+          <button className=" group text-white border-2 px-6 py-4 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300"><HiArrowNarrowRight className="ml-2" /></span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
